150e27a0f6492a7002e75742ac47f187b2eab90b
akshaykashyap27 <88394943+akshaykashyap27@users.noreply.github.com>
2021-08-18 16:17:07 +0530

    Update Main.py

--- Practical_Test/Main.py
+++ Practical_Test/Main.py
@@ -1,5 +1,7 @@
+from compareFunc import compareStrength
 from Class_Enemy import Enemy
 from Class_Heroes import Hero
+
 
 strength=Enemy.EnemyMap
 energy=Hero.HeroMap
@@ -8,20 +10,8 @@ energy=Hero.HeroMap
 
 if (len(strength) != len(energy)):
     raise Exception("Number of heroes and enemies don't match")
-def compareStrength(strength,energy):
-    if (energy == strength):
-        print("Both are equal")
 
-    else:
-        count=0
-        for i in range(len(energy)):
-            if (energy[i] >= strength[i]):
-                count += 1
-
-        if (count == len(energy)):
-            print("Result - WIN")
-
-        else:
-            print("Result - LOSE")
-
-compareStrength(strength,energy)
+else:
+    compareStrength(strength, energy)
+    print(strength)
+    print(energy)
